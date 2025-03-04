f6260ccef75af1723d9a06da08cf1d19d2a2e946
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-03-04 21:22:18 +0530

    Implement PPR and Create Query to fetch startups by ID

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,25 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    dangerouslyAllowSVG: true, // TODO: REMOVE THIS BEFORE PRODUCTION
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*",
-      },
-    ],
-  },
+    /* config options here */
+    images: {
+        dangerouslyAllowSVG: true, // TODO: REMOVE THIS BEFORE PRODUCTION
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "*",
+            },
+        ],
+    },
+    experimental: {
+        ppr: "incremental" // ppr => PARTIAL PRE RENDERING
+    },
+    // HELPS TO VISUALIZE THE BUILD ACTIVITY
+    // devIndicators:{
+    //     appIsrStatus: true,
+    //     buildActivity: true,
+    //     buildActivityPosition: "bottom-right"
+    // }
 };
 
 export default nextConfig;
