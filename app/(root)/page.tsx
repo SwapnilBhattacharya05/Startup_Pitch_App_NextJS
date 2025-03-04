882e8fc9eb4e7c961120d7cb144cc6502c457550
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-03-04 17:02:12 +0530

    Implement Real Time Search with URL Query Params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,65 +1,64 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 // import { client } from "@/sanity/lib/client";
-import { sanityFetch, SanityLive } from "@/sanity/lib/live";
-import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import {sanityFetch, SanityLive} from "@/sanity/lib/live";
+import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ query: string }>;
-}) {
-  const query = (await searchParams).query;
+export default async function Home({searchParams}: { searchParams: Promise<{ query: string }> }) {
+    const query = (await searchParams).query;
 
-  // const posts = await client.fetch(STARTUPS_QUERY);
+    // RETRIEVE DATA FROM SANITY
+    const params = {search: query || null};
 
-  // ?THIS NEW CHANGE REVALIDATES THE PAGE WHENEVER THE NEW CHANGES ARE MADE
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+    // const posts = await client.fetch(STARTUPS_QUERY);
 
-  // console.log(JSON.stringify(posts, null, 2));
+    // ?THIS NEW CHANGE REVALIDATES THE PAGE WHENEVER THE NEW CHANGES ARE MADE
+    const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
-  // const posts = [
-  //   {
-  //     _id: 1,
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 1, name: "Swapnil" },
-  //     description: "Lorem ipsum dolor sit amet.",
-  //     image:
-  //       "https://images.unsplash.com/photo-1589254065878-42c9da997008?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  // ];
+    // console.log(JSON.stringify(posts, null, 2));
 
-  return (
-    <>
-      <section className="pink_container">
-        <h1 className="heading">
-          Pitch Your Startup, <br />
-          Connect with Entrepreneurs
-        </h1>
-        <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
-        </p>
-        <SearchForm query={query} />
-      </section>
-      <section className="section_container">
-        <p className="text-30-semibold">
-          {query ? `Search results for "${query}"` : "All Startups"}
-        </p>
-        <ul className="mt-7 card_grid">
-          {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard) => (
-              <StartupCard key={post._id} post={post} />
-            ))
-          ) : (
-            <p className="no-result">No results found</p>
-          )}
-        </ul>
-      </section>
+    // const posts = [
+    //   {
+    //     _id: 1,
+    //     _createdAt: new Date(),
+    //     views: 55,
+    //     author: { _id: 1, name: "Swapnil" },
+    //     description: "Lorem ipsum dolor sit amet.",
+    //     image:
+    //       "https://images.unsplash.com/photo-1589254065878-42c9da997008?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    //     category: "Robots",
+    //     title: "We Robots",
+    //   },
+    // ];
 
-      <SanityLive />
-    </>
-  );
+    return (
+        <>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch Your Startup, <br/>
+                    Connect with Entrepreneurs
+                </h1>
+                <p className="sub-heading !max-w-3xl">
+                    Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+                </p>
+                <SearchForm query={query}/>
+            </section>
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : "All Startups"}
+                </p>
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupTypeCard) => (
+                            <StartupCard key={post._id} post={post}/>
+                        ))
+                    ) : (
+                        <p className="no-result">No results found</p>
+                    )}
+                </ul>
+            </section>
+
+            <SanityLive/>
+        </>
+    );
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,16 @@
-import { defineQuery } from "next-sanity";
+import {defineQuery} from "next-sanity";
 
 export const STARTUPS_QUERY =
-  defineQuery(` *[_type == "startup" && defined(slug.current)] | order(_createdAt desc){
+    /*
+    * CHECKING WHETHER SEARCH EXISTS
+    * OR
+    * SEARCH MATCHES THE TITLE
+    * OR
+    * SEARCH MATCHES THE CATEGORY
+    * OR
+    * SEARCH MATCHES THE AUTHOR'S NAME
+    */
+    defineQuery(` *[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author ->name match $search] | order(_createdAt desc){
   _id,
   title,
   slug,
